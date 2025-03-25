d94293ed4f3d7337a9b1accdff3bf7c09b3499ba
lecksantos <lecksantos98@gmail.com>
2025-03-25 00:07:41 +0000

    adicionando o tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,7 +5,22 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
     // Nível Novato - Posicionamento dos Navios
+    printf("Tabuleiro:\n");
+    char linha[10]= {'A','B','C','D','E','F','G','H','I','J'};
+    for (int j = 0; j < 10; j++)
+    {
+        printf("%c ", linha[j]);
+    }printf("\n");
+    
+    int tabuleiro[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                printf("%d ", tabuleiro[i][j] = 0);  // Água
+            }
+            printf("\n");   
+        }
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
